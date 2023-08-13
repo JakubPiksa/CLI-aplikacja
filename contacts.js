d4a4b0e13ba1ec55674eec197eeab02aabf2e20d
Jakub Piksa <kuba.piksa@wp.pl>
2023-08-13 17:13:12 +0200

    Update contacts.js

---
+++ contacts.js
@@ -0,0 +1,25 @@
+/*
+ * Skomentuj i zapisz wartość
+ * const contactsPath = ;
+ */
+import fs from 'fs';
+import path from 'path';
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+  // ...twój kod
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}
